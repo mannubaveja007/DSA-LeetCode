5f226f5b08e6292c43e7e5d2e2dd3208359bc40c
Shipra Gupta <gshipra099@gmail.com>
2025-10-30 18:42:23 +0530

    added array maximum and minimum code

---
+++ cpp/arrays/MaxMin_ShipraGupta.cpp
@@ -0,0 +1,28 @@
+// Program: Find Maximum and Minimum Element in an Array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    cout << "\nMaximum Element: " << max;
+    cout << "\nMinimum Element: " << min << endl;
+
+    return 0;
+}
